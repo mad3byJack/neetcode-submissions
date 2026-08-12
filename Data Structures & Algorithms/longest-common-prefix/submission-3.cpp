8a9ebbe07f15e870a99c207055a3eb3e71b4251b
Jack <152612532+mad3byJack@users.noreply.github.com>
2026-08-12 23:22:17 +1000

    Add: longest-common-prefix - submission-3

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-3.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if (strs.empty()) return "";
+        if (strs.size() == 1) return strs[0];
+        std::string ans = "";
+        int str = 1;
+        int letter = 0;
+        while (letter < strs[0].length()) {
+            char curr = strs[0][letter];
+            if (str < strs.size() && curr == strs[str][letter]) {
+                if (str == strs.size() - 1) {
+                    ans.push_back(curr);
+                    letter++;
+                    str = 1;
+                } else {
+                    str++;
+                }
+            } else {
+                break;
+            }
+        }
+        return ans;
+    }
+};
